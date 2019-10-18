46afc8652184a83f783f2672a5aa464b8913e08d
Albert <42963174+albertZubov@users.noreply.github.com>
2019-10-18 13:11:55 +0300

    last

--- gulpfile.js
+++ gulpfile.js
@@ -22,10 +22,7 @@ var deploy = require('gulp-gh-pages');
 /**
  * Push build to gh-pages
  */
-gulp.task('deploy', function () {
-  return gulp.src("build/**/*")
-    .pipe(deploy())
-});
+gulp.task('deploy', () => gulp.src("./build/**/*").pipe(deploy()));
 
 
 gulp.task("css", function () {
